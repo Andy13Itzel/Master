d32810958b20ba8bf8fcd279b426b34809a00d0e
Andrea Juarez <andreai.juarezr13@gmail.com>
2021-07-12 22:03:16 -0500

    final

--- Clases/Intro JS/09. Programación orientada a objetos/index.js
+++ Clases/Intro JS/09. Programación orientada a objetos/index.js
@@ -65,7 +65,7 @@ console.log(EstudianteDos.nombre, EstudianteDos.apellido, EstudianteDos.Aprobado
 // VAMOS A TENER UNA CLASE:  DISPONIBILIDAD
 //CREAR NUESTRA PLANTILLA
 //SOLO UN CONSTRUCTOR POR CLASE
-
+/* 
 class Cartelera {
     //CREAR NUESTRO CONSTRUCTOR
     constructor(nombre, sala, clasificacion, disponible){
@@ -95,4 +95,38 @@ const peliDos = new Cartelera ("\nPELICULA: JHON WICK", "\nSALA: 2", "\nCLASIFIC
 
 console.log("En carterlera: ",peliUno.nombre, peliUno.sala, peliUno.clasificacion, peliUno.Disponible())
 
-console.log("En carterlera: ",peliDos.nombre, peliDos.sala, peliDos.clasificacion, peliDos.Disponible())
+console.log("En carterlera: ",peliDos.nombre, peliDos.sala, peliDos.clasificacion, peliDos.Disponible()) */
+
+    
+
+class Consola{
+    constructor(nombre, videojuegos, disponibilidad, precio){
+        this.nombre=nombre
+        this.videojuegos=videojuegos
+        this.disponibilidad=disponibilidad
+        this.precio=precio
+    }
+    Disponible(){
+        if(this.disponibilidad){
+            return (`La Consola ${this.nombre} esta disponible actualmente. Por un precio de $${this.precio}`)
+        }else{
+            return (`La Consola ${this.nombre} no esta disponible actualmente`)
+        }
+    }
+    Incluye(){
+        if(this.disponibilidad){
+            return (`La consola ${this.nombre} incluye los juegos ${this.videojuegos[0]}, ${this.videojuegos[1]}, ${this.videojuegos[2]}`)
+        }else{
+            return (`Me encantaria decirte que juegos se incluyen, pero la consola no esta disponible 😔`)
+        }
+    }
+}
+let xboxOne=new Consola("Xbox One", ["Grand Theft Auto", "Naruto Storm", "Fifa 21"], true, "200")
+let xboxSeriesX=new Consola("Xbox Series X", ["Farcry", "Warzone", "Fifa 21"], false, "500")
+let xboxSeriesS=new Consola("Xbox Series S", ["Final Fantasy", "Farm Simulator", "Fifa 21"], true, "300")
+
+var resultado = xboxOne.Incluye()
+var resultado2 = xboxOne.Disponible()
+
+console.log(resultado)
+console.log(resultado2)
