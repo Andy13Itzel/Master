87c6562b20ac9a33c8d6f92a2134870a6021ba20
Andrea Juarez <andreai.juarezr13@gmail.com>
2021-07-18 00:26:09 -0500

    cajero

---
+++ Proyectos/Modulo_2/index.js
@@ -0,0 +1,14 @@
+
+var cuentas = [
+    { 1: "Mali", saldo: 200, password: '1825' },
+    { 2: "Gera", saldo: 290, password: 'l334' },
+    { 3: "Maui", saldo: 67, password: '1234' }
+  ];
+  
+
+if (cuentas === 1 ) {
+    "ingrese su password"
+    
+} else {
+    
+}
